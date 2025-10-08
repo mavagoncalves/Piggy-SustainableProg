b34b0ab3ba3a298bff4e68bdae977b27e010ca9c
Roberto Rodriguez Espejo <rrodriguezesp@yahoo.es>
2025-10-08 14:51:53 +0200

    added def.face (for icon) and def turn(for result output)

--- src/dice.py
+++ src/dice.py
@@ -10,3 +10,16 @@ class Dice:
         self.icon=self.face
         return self.value
 
+    def face(self):
+        icon = {
+            1: "⚀",
+            2: "⚁",
+            3: "⚂",
+            4: "⚃",
+            5: "⚄",
+            6: "⚅"
+        }
+        return icon[self.value]
+
+    def turn(self):
+        print(f"Result: {self.face()} -> {self.value}")
