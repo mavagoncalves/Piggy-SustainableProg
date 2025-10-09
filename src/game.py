df09705a5ace9e0de70df73e7c65092b6745d6ba
Roberto Rodriguez Espejo <rrodriguezesp@yahoo.es>
2025-10-09 18:57:53 +0200

    bases for plays_turn function

--- src/game.py
+++ src/game.py
@@ -4,4 +4,19 @@ from player import Player
 
 class Game:
     def __init__(self):
-        pass
+        self.round_score = 0
+        self.dice=Dice(6)
+        self.player1=Player(Player.name) #AQUI EL NOMBRE
+        self.player2=Player(Player.name) #AQUI EL OTRO NOMBRE
+        self.current_player=self.player1 #FIRST TURN ASSIGNED ALWAYS TO PLAYER 1, THEN CHANGES
+
+    def plays_turn(self):
+        self.round_score=0
+        print(f"It is {self.current_player}'s turn.")
+        self.dice.roll() #ROLLS THE DICE AND GETS A VALUE
+        print(f"{self.current_player} rolled a {self.dice.turn()}") #PRINTS THE VALUE + ICON
+        if self.dice.roll()!=1:
+            self.round_score += self.dice.roll()
+        else:
+            print(f"{self.player1} lost the score!")
+            self.current_player=self.player2
