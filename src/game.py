176359ff0a10ea1ba98a51c5fe296b9aa3d8222b
Roberto Rodriguez Espejo <rrodriguezesp@yahoo.es>
2025-10-09 19:04:42 +0200

    Added change player function, forgot about it :S

--- src/game.py
+++ src/game.py
@@ -6,17 +6,28 @@ class Game:
     def __init__(self):
         self.round_score = 0
         self.dice=Dice(6)
-        self.player1=Player(Player.name) #AQUI EL NOMBRE
-        self.player2=Player(Player.name) #AQUI EL OTRO NOMBRE
+        self.player1=Player("NOMBRE1") #AQUI EL NOMBRE
+        self.player2=Player("NOMBRE2") #AQUI EL OTRO NOMBRE
         self.current_player=self.player1 #FIRST TURN ASSIGNED ALWAYS TO PLAYER 1, THEN CHANGES
 
     def plays_turn(self):
         self.round_score=0
         print(f"It is {self.current_player}'s turn.")
-        self.dice.roll() #ROLLS THE DICE AND GETS A VALUE
-        print(f"{self.current_player} rolled a {self.dice.turn()}") #PRINTS THE VALUE + ICON
-        if self.dice.roll()!=1:
-            self.round_score += self.dice.roll()
-        else:
-            print(f"{self.player1} lost the score!")
+
+        #TURN STARTS
+        while True:
+            input("Press enter to continue or whatever")  #PREGUNTAR SI EL NUMERO ES INPUT O UN COMANDO COMO EN EL MENU
+            roll=self.dice.roll() #ROLLS THE DICE AND GETS A VALUE
+            print(f"{self.current_player} rolled a {self.dice.turn()}") #PRINTS THE VALUE + ICON
+            if self.dice.roll()!=1:
+                self.round_score += self.dice.roll()
+            else:
+                print(f"{self.player1} lost the score!")
+                self.current_player=self.player2
+
+
+    def change_player(self, player):
+        if self.current_player==self.player1:
             self.current_player=self.player2
+        else:
+            self.current_player=self.player1
