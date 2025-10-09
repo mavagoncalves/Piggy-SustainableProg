9fe2d1fae5037c802b9d45f7856babbe1866b213
mavagoncalves <mavagoncalves@gmail.com>
2025-10-09 17:01:09 +0200

    medium difficulty ai logic

--- src/ai.py
+++ src/ai.py
@@ -30,4 +30,28 @@ class AI:
             return 'hold'
         return 'roll' if random.random() > 0.6 else 'hold'
     
+    def medium(self, myscore, opponent_score, turn_score):
+        '''
+        Medium AI:
+        * keep limit at 20
+        * if ahead by +20 or more take safer turn (hold at 15)
+        * if behind by -30 or more take riskier turn (hold at 25)
+        '''
+
+        lead = myscore - opponent_score
+        limit = 20
+
+        if myscore + turn_score >= self.goal:
+            return 'hold'
+        
+        if lead >= limit:
+            limit = 15
+        
+        if lead <= (-30):
+            limit = 25
+
+        if turn_score >= limit:
+            return 'hold'
+        return 'roll'
+    
 
