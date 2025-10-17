be3ebe1a3ddb6e60a239c32c24e715d67dd56ca3
ayahassaad <ayah.assaad@icloud.com>
2025-10-17 13:14:01 +0200

    Menu Display

--- src/menu.py
+++ src/menu.py
@@ -2,3 +2,14 @@ class Menu:
     def __init__(self):
         self.running = True
 
+    def display(self):
+        print("\n=== MAIN MENU ===")
+        print("1. Rules")
+        print("2. New Game")
+        print("3. Change Name")
+        print("4. High Score")
+        print("5. Quit")
+
+
+
+
