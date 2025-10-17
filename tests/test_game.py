579159036504d0c6e17ae8a5597b7603c3cb0f6e
ayahassaad <ayah.assaad@icloud.com>
2025-10-17 15:46:08 +0200

    test change player switches between players

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,3 +1,21 @@
+from src import game
+
+from src.player import Player
+
+
+class TestGame(game.Game):
+    def __init__(self):
+        super().__init__()
+
+def test_change_player_switches_between_players():
+    g = TestGame()
+    g.player1 = Player("Player1")
+    g.player2 = Player("Player2")
+    g.current_player = g.player1
+    g.change_player()
+    assert g.current_player is g.player2
+    g.change_player()
+    assert g.current_player is g.player1
 
 
 
