0c1a442727f604e61216c3e946c66c73a6933e49
mavagoncalves <mavagoncalves@gmail.com>
2025-10-18 01:23:12 +0200

    re making load function

--- src/highscore.py
+++ src/highscore.py
@@ -4,3 +4,13 @@ import os
 class HighScore:
     def __init__(self, file_path=os.path.join("data", "highscore.json")):
         self.file = file_path
+    
+    def load(self):
+        if not os.path.exists(self.file):
+            return []
+        with open(self.file, "r") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+
