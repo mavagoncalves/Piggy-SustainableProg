f59ea416c0f2016cd1eee5354cc21f2dadb31784
ayahassaad <ayah.assaad@icloud.com>
2025-10-17 13:30:12 +0200

    def run

--- src/menu.py
+++ src/menu.py
@@ -10,6 +10,28 @@ class Menu:
         print("4. High Score")
         print("5. Quit")
 
+    def run(self):
+        while self.running:
+            self.display()
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                print("Rules:")
+                #Connect to rules here when ready
+            elif choice == "2":
+                print("Loading Game...")
+                #Connect to game class when ready
+            elif choice == "3":
+                print("Change Name:")
+                #Connect to name class when ready
+            elif choice == "4":
+                print("High Score:")
+                #Connect to high score class when ready
+            elif choice == "5":
+                print("Quitting...")
+                self.running = False
+
+
 
 
 
