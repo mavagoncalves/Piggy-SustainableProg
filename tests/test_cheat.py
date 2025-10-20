fd28b1154f2b9a10fe04c10e954cb3c27f570418
ayahassaad <ayah.assaad@icloud.com>
2025-10-20 14:55:03 +0200

    Test: Different cheat objects are independent

--- tests/test_cheat.py
+++ tests/test_cheat.py
@@ -54,3 +54,8 @@ def test_game_can_be_any_object():
     g = DummyGame()
     cheat = Cheat(g)
     assert cheat.game is g
+
+def test_multiple_cheat_objects_independent():
+    c1 = Cheat("game1")
+    c2 = Cheat("game2")
+    assert c1.game != c2.game
