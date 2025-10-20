6123cdeabe0b790efeb59ab270900948efc02d17
ayahassaad <ayah.assaad@icloud.com>
2025-10-20 11:58:37 +0200

    Test: Save & Load

--- tests/test_highscore.py
+++ tests/test_highscore.py
@@ -3,3 +3,11 @@ from src.highscore import HighScore
 def test_load_returns_empty():
     hs = HighScore()
     assert hs.load() == []
+
+def test_save_and_load():
+    hs = HighScore()
+    data = [["Alice", 100]]
+    hs.save(data)
+    result = hs.load()
+    assert result == data
+
