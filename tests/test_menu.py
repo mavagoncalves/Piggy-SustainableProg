c70673a43661560329af8887ce03c155ead96c2c
Roberto Rodriguez Espejo <rrodriguezesp@yahoo.es>
2025-10-21 11:10:28 +0200

    Wrote a test for input as int

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -2,6 +2,11 @@ from src.menu import Menu
 from unittest.mock import patch
 import pytest
 
+
+    #   Command for Windows
+    #   $env:PYTHONPATH="."; .venv\Scripts\pytest.exe -v
+
+
 def test_menu_just_runs():
     menu=Menu()
     menu.display()
@@ -15,4 +20,10 @@ def test_menu_string_input_is_invalid():
     with patch("builtins.input", side_effect=["r","5"]):
         # "r" as invalid, gets the invalid input, and gets back
         # "5" to get out of the loop
+        menu.run()
+
+def test_menu_int_input_not_recognized():
+    menu=Menu()
+    with patch("builtins.input", side_effect=[1,"5"]):
+        # same as previous
         menu.run()
